8687b0b26a156af71b170e8e5a8afe41edf64370
DanielDitrikh <daniel.ditrikh@mail.ru>
2024-12-26 22:57:05 +0300

    fd

--- EX1/apptest/models.py
+++ EX1/apptest/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+
 from django.core.validators import MinValueValidator
 class Product(models.Model):
     name = models.CharField(max_length=50, unique=True,)
@@ -9,6 +10,7 @@ class Product(models.Model):
 
     def __str__(self):
         return  f'{self.name.title()}: {self.description[:20]}'
+
 
 class Category(models.Model):
     name = models.CharField(max_length=150, unique=True,)
